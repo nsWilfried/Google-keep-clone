7f1f587d728d66f59c081a245fb2db665222457a
Wilfried N'SOUKPOE <wilfriednsoukpoe1@gmail.com>
2022-08-20 10:05:27 +0000

    Add, remove item

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,52 @@
 <template>
     <div>
-        <span>hello world</span>
+       <div>
+        <label for="title">Titre</label>
+        <input type="text" v-model="title"  name="name">
+       </div>
+       <div>
+            <label for="title">Contenu</label>
+            <input type="text" v-model="message"  name="name">
+       </div>
+       <div>
+        <button v-on:click="addElement()">Ajouter</button>
+       </div>
+
+       <div v-for="(item, index) in data" :key="index">
+            <span>
+                {{item.title}}
+            </span>
+            <button v-on:click="removeItem()">Supprimer</button>
+       </div>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            users: []
+            title: '', 
+            message: '',
+            data: []
         }
     }, 
+    methods: {
+        addElement() {
+           this.data.push({
+            title: this.title, 
+            message: this.message
+           }
+           )
+           this.clearInputs()
+        }, 
+
+        clearInputs(){
+            this.title ='', 
+            this.message =''
+        }, 
+        removeItem(){
+            this.data.splice(this.data.indexOf(this), 1)
+        },
+    }
 }
 </script>
 <style lang="">
